602bba426e4bd14607ad870f639a77aea6d2aae6
J2b2590 <J2b2590@gmail.com>
2020-10-06 11:41:57 -0500

    finished lab

--- app.rb
+++ app.rb
@@ -4,5 +4,19 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get "/hello" do
+		"Testing the info page"
+		erb :hello
+	  end 
+
+	  get "/goodbye" do
+		"Testing the info page"
+		erb :goodbye
+	  end 
+
+	  get "/date" do
+		erb :date
+	  end
+
 
 end
